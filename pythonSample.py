da83ce1d9b36a2f250b3a88f9fcca2f550264ebf
Jeff Bankston <jeff986@comcast.net>
2023-12-04 21:16:25 -0600

    Committed all

---
+++ pythonSample.py
@@ -0,0 +1,5 @@
+print("------------------------------------------------")
+print("Python has entered the building")
+print("Let the party begin!")
+print("Jeff Bankston")
+print("------------------------------------------------")
