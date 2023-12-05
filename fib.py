5c9b9108b8ffda9730c62bcc8d51512f76c7cd69
Jeff Bankston <jeff986@comcast.net>
2023-12-04 20:53:24 -0600

    first commit

---
+++ fib.py
@@ -0,0 +1,21 @@
+# recursive approach
+numTerms = int(input("How many terms of Fibonacci sequence ro print? "))
+
+# What are the first few terms of the fib sequence
+# 0 1 1 2 3
+
+# main method
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return (fibonacci(n-1) + (fibonacci(n-2)))
+    
+    
+# check if the number of terms is valid
+if numTerms <= 0:
+    print("Please enter a positive integer.")
+else:
+    print("Fibonacci sequence")
+    for i in range(numTerms):
+        print(fibonacci(i))
